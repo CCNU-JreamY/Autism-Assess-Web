5f62fe75f26c3203c3b8143d80cfeb9708e0e6f5
殷纪民 <yinjimin@iboxpay.com>
2021-05-27 18:11:16 +0800

    纠错

---
+++ src/main/java/cn/pavi/aaw/bean/response/DataResponse.java
@@ -0,0 +1,27 @@
+package cn.pavi.aaw.bean.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Map;
+
+/**
+ * @Description: 可返回Map数据返回体类
+ * @Author: JreamY
+ * @Date: 2021/5/27
+ **/
+@Setter
+@Getter
+@ToString
+public class DataResponse extends Response {
+
+    private Map<String, Object> data;
+
+    public static DataResponse newSuccess(Map<String, Object> data) {
+        Response response = Response.newSuccess();
+        ((DataResponse) response).setData(data);
+        return (DataResponse) response;
+    }
+
+}
